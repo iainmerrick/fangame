dc698023674bb7ba69d552c27bdd00bad452ea2d
Iain Merrick <iain@iainmerrick.com>
2018-05-17 00:23:23 +0100

    Make FileLoader asynchronous. Test still works.

--- src/Model/FileLoader.m
+++ src/Model/FileLoader.m
@@ -1,6 +1,16 @@
 #import "FileLoader.h"
 
+static dispatch_queue_t _background_queue;
+
 @implementation FileLoader
+
++ (void)initialize
+{
+	if (self == FileLoader.class) {
+		_background_queue =
+			dispatch_queue_create("FileLoader", DISPATCH_QUEUE_CONCURRENT);
+	}
+}
 
 - (instancetype)initWithPath:(NSString*)path
 {
@@ -14,16 +24,22 @@
 - (void)loadUrl:(NSString*)url completion:(LoadCompletionBlock)block
 {
 	NSString* path = [_path stringByAppendingPathComponent:url];
-	NSError* err;
-	NSData* data = [NSData dataWithContentsOfFile:path
-										  options:NSDataReadingMappedIfSafe
-											error:&err];
-	if (err) {
-		block(nil, nil, err);
-	}
-	NSString* dir = path.stringByDeletingLastPathComponent;
-	FileLoader* subloader = [FileLoader loaderWithPath:dir];
-	block(data, subloader, nil);
+	dispatch_async(_background_queue, ^{
+		NSError* err;
+		NSData* data = [NSData dataWithContentsOfFile:path
+											  options:NSDataReadingMappedIfSafe
+												error:&err];
+		if (err) {
+			dispatch_async(dispatch_get_main_queue(), ^{
+				block(nil, nil, err);
+			});
+		}
+		NSString* dir = path.stringByDeletingLastPathComponent;
+		FileLoader* subloader = [FileLoader loaderWithPath:dir];
+		dispatch_async(dispatch_get_main_queue(), ^{
+			block(data, subloader, nil);
+		});
+	});
 }
 
 + (instancetype)loaderWithPath:(NSString*)path
